67dc31f237c7ad3761393078a4ed872d3f375c46
shijingyu <945090896@qq.com>
2017-06-19 18:21:49 +0800

    添加多页面和多条目

--- scrapyspider/spiders/blog_spider.py
+++ scrapyspider/spiders/blog_spider.py
@@ -1,15 +1,30 @@
-#-*- coding:UTF-8 -*-
-from scrapy.spiders import Spider
+# -*- coding:UTF-8 -*-
+import scrapy
+
 import sys
+
 reload(sys)
-sys.setdefaultencoding( "utf-8" )
+sys.setdefaultencoding("utf-8")
+from scrapyspider.items import TestSpiderItem
 
 
-class BlogSpider(Spider):
-    name = 'shitouboy'
-    start_urls = ['https://blog.shitouboy.com']
+class Blog_spider(scrapy.Spider):
+    name = "shitou_blog"
+    start_urls = ['http://blog.shitouboy.com',
+                  'http://blog.shitouboy.com/index2.html',
+                  'http://blog.shitouboy.com/index3.html',
+                  'http://blog.shitouboy.com/index4.html',
+                  'http://blog.shitouboy.com/index5.html',
+                  ]
 
     def parse(self, response):
-        titles = response.xpath('//h2[@class="post-title"]/text()').extract()
-        for title in titles:
-            print title.strip()
+        i = 0
+        for sel in response.xpath('//div[@class="post-preview"]'):
+            item = TestSpiderItem()
+            item['title'] = sel.xpath('//h2[@class="post-title"]/text()')[i].extract()
+            item['desc'] = sel.xpath('//p[@class="article-summary"]/text()')[i].extract()
+            item['author'] = sel.xpath('//p[@class="post-meta"]/text()')[i].extract()
+            i += 1
+            yield item
+
+
